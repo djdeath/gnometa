79300b600cef2b03fd4ac22476c1dafb590db04f
Lionel Landwerlin <llandwerlin@gmail.com>
2014-10-28 23:14:47 +0000

    regenerate compiler using optimization

--- bs-ometa-js-compiler.js
+++ bs-ometa-js-compiler.js
@@ -1,3 +1,3 @@
 let BSOMetaJSParser=objectThatDelegatesTo(BSJSParser,{
-"primExprHd":function(){var $elf=this,_fromIdx=this.input.idx,r;return this._or((function(){return (function(){r=this._applyWithArgs("foreign",BSOMetaParser,'grammar');return r;}).call(this);}),(function(){return BSJSParser._superApplyWithArgs(this,'primExprHd');}));}});let BSOMetaJSTranslator=objectThatDelegatesTo(BSJSTranslator,{
+"primExprHd":function(){var $elf=this,_fromIdx=this.input.idx,r;return this._or(function(){r=this._applyWithArgs("foreign",BSOMetaParser,'grammar');return r;},function(){return BSJSParser._superApplyWithArgs(this,'primExprHd');});}});let BSOMetaJSTranslator=objectThatDelegatesTo(BSJSTranslator,{
 "Grammar":function(){var $elf=this,_fromIdx=this.input.idx;return this._applyWithArgs("foreign",BSOMetaTranslator,'Grammar');}})
