fd977e59ca7c32b48f2b6f72d7cf92ba5e1845a4
Lionel Landwerlin <llandwerlin@gmail.com>
2015-03-29 00:12:14 +0000

    compiler: embed rule variables into single object

--- bs-ometa-js-compiler.js
+++ bs-ometa-js-compiler.js
@@ -2,5 +2,5 @@
 // https://github.com/djdeath/gnometa
 
 let BSOMetaJSParser=objectThatDelegatesTo(BSJSParser,{
-"primExprHd":function(){var $elf=this,_fromIdx=this.input.idx,r;return this._or(function(){r=this._applyWithArgs("foreign",BSOMetaParser,'grammar');return r;},function(){return BSJSParser._superApplyWithArgs(this,'primExprHd');});}});let BSOMetaJSTranslator=objectThatDelegatesTo(BSJSTranslator,{
-"Grammar":function(){var $elf=this,_fromIdx=this.input.idx;return this._applyWithArgs("foreign",BSOMetaTranslator,'Grammar');}})
+"primExprHd":function(){var $elf=this, $vars={};return this._or(function(){$vars.r=this._applyWithArgs("foreign",BSOMetaParser,'grammar');return $vars.r;},function(){return BSJSParser._superApplyWithArgs(this,'primExprHd');});}});let BSOMetaJSTranslator=objectThatDelegatesTo(BSJSTranslator,{
+"Grammar":function(){var $elf=this, $vars={};return this._applyWithArgs("foreign",BSOMetaTranslator,'Grammar');}})
