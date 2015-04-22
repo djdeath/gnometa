2ac9ffadd48ed33ddfb1f0ea1790a87fa7af7ada
Lionel Landwerlin <llandwerlin@gmail.com>
2015-04-22 18:42:15 +0100

    stash....

--- ui/TextView.js
+++ ui/TextView.js
@@ -1,53 +1,85 @@
+const Lang = imports.lang;
 const Gio = imports.gi.Gio;
 const GObject = imports.gi.GObject;
 const Gdk = imports.gi.Gdk;
 const Gtk = imports.gi.Gtk;
 const GtkSource = imports.gi.GtkSource;
 
-let TextView = function() {
-  this._init();
-};
-
 let allocationBoxToString = function(box) {
     return '' + box.width + 'x' + box.height + ' @ ' + box.x + 'x' + box.y;
 };
 
-TextView.prototype = {
+const TextView = new Lang.Class({
+  Name: 'TextView',
+  Extends: GtkSource.View;
+  Signals: {
+    'location-changed': { }
+  },
+
+
   _init: function() {
-    this._widget = new GtkSource.View({
-      show_line_numbers: true,
-    });
-    this._buffer = this._widget.buffer;
+    this.show_line_numbers = true;
 
     let lang_manager = GtkSource.LanguageManager.get_default();
-    this._buffer.set_language(lang_manager.get_language('js'));
+    this.buffer.set_language(lang_manager.get_language('js'));
 
-    let tag_table = this._buffer.get_tag_table();
+    let tag_table = this.buffer.get_tag_table();
     let tag = new Gtk.TextTag({
       name: 'highlight',
       background: 'lightblue',
     });
     tag_table.add(tag);
+
+    this._setInControl(false);
+    this.connect('key-press-event', this._keyPressed.bind(this));
+    this.connect('key-release-event', this._keyReleased.bind(this));
+    this.connect('motion-notify-event', this._motionNotified.bind(this));
   },
 
-  getWidget: function() {
-    return this._widget;
+  _setInControl: function(value) {
+    this._inControl = value;
   },
+  _getInControl: function() {
+    return this._inControl;
+  },
+
+  _keyPressed: function(widget, event) {
+    let keyval = event.get_keyval()[1];
+    switch (keyval) {
+    case Gdk.KEY_Control_L: _setInControl(true); break;
+    }
+    return false;
+  },
+  _keyReleased: function(widget, event) {
+    let keyval = event.get_keyval()[1];
+    switch (keyval) {
+    case Gdk.KEY_Control_L: _setInControl(false); break;
+    }
+    return false;
+  },
+  _motionNotified: function(widget, event) {
+    if (!this._getInControl()) return false;
+    let [, x, y] = event.get_coords(),
+        offset = textview.getOffsetAtLocation(x, y);
+
+    return false;
+  },
+
 
   getOffsetAtLocation: function(x, y) {
-    x += this._widget.get_hadjustment().get_value();
-    y += this._widget.get_vadjustment().get_value();
-    let iter = this._widget.get_iter_at_location(x, y);
+    x += this.get_hadjustment().get_value();
+    y += this.get_vadjustment().get_value();
+    let iter = this.get_iter_at_location(x, y);
 
     return iter.get_offset();
   },
 
   getRectForRange: function(start, stop) {
-    let start_iter = this._buffer.get_iter_at_offset(start);
-    let end_iter = this._buffer.get_iter_at_offset(stop);
+    let start_iter = this.buffer.get_iter_at_offset(start);
+    let end_iter = this.buffer.get_iter_at_offset(stop);
 
-    let rect1 = this._widget.get_iter_location(start_iter),
-        rect2 = this._widget.get_iter_location(end_iter);
+    let rect1 = this.get_iter_location(start_iter),
+        rect2 = this.get_iter_location(end_iter);
     let ret =  Gdk.rectangle_union(rect1, rect2);
     ret.x += 20; // GtkSourceView sucks.
     return ret;
@@ -57,15 +89,15 @@ TextView.prototype = {
     let start_iter, end_iter;
 
     if (this._highlight) {
-      start_iter = this._buffer.get_iter_at_offset(this._highlight.start);
-      end_iter = this._buffer.get_iter_at_offset(this._highlight.stop);
-      this._buffer.remove_tag_by_name('highlight',
+      start_iter = this.buffer.get_iter_at_offset(this._highlight.start);
+      end_iter = this.buffer.get_iter_at_offset(this._highlight.stop);
+      this.buffer.remove_tag_by_name('highlight',
                                       start_iter, end_iter);
     }
 
-    start_iter = this._buffer.get_iter_at_offset(start);
-    end_iter = this._buffer.get_iter_at_offset(stop);
-    this._buffer.apply_tag_by_name('highlight', start_iter, end_iter);
+    start_iter = this.buffer.get_iter_at_offset(start);
+    end_iter = this.buffer.get_iter_at_offset(stop);
+    this.buffer.apply_tag_by_name('highlight', start_iter, end_iter);
 
     this._highlight = {
       start: start,
@@ -77,17 +109,17 @@ TextView.prototype = {
     if (!(typeof data === 'string'))
       throw new Error('Cannot display an object other than a string');
     this._data = data;
-    this._buffer.set_text(data, -1);
+    this.buffer.set_text(data, -1);
   },
 
   onChange: function(callback) {
-    this._buffer.connect('changed', function() {
-      callback(this._buffer.get_text(this._buffer.get_start_iter(),
-                                     this._buffer.get_end_iter(),
-                                     true));
+    this.buffer.connect('changed', function() {
+      callback(this.buffer.get_text(this.buffer.get_start_iter(),
+                                    this.buffer.get_end_iter(),
+                                    true));
     }.bind(this));
   },
-};
+});
 
 
 const TEST = false;
@@ -96,7 +128,7 @@ if (TEST) {
 
   let scrolled = new Gtk.ScrolledWindow();
   let view = new TextView();
-  scrolled.add(view.getWidget());
+  scrolled.add(widget);
   let file = Gio.File.new_for_path('./lr.ometajs');
   let [, data] = file.load_contents(null);
   let source = '' + data;
